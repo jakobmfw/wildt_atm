c4fd333841625f5e84f9452c709a65c7222c80c6
Jakob Wildt <jakob.wildt@gmx.at>
2020-05-01 16:13:34 +0200

    automaton nearly works, boost was used properly now

--- code/include/automaton.h
+++ code/include/automaton.h
@@ -4,12 +4,27 @@
 #pragma once
 #include "boost/include/sml.hpp"
 #include <iostream>
+#include <map>
+#include <string>
 
-using namespace std;
 namespace sml = boost::sml;
 
-
 namespace{
+
+std::map<std::string, int> cards = {
+    { "karte1", 1234 },
+    { "karte2", 6969 },
+    { "karte3", 0420 }
+};
+
+std::string karte;
+int pin_;
+int aktion;
+int geldmenge;
+
+float kontostand1{69420};
+float kontostand2{187};
+float kontostand3{99.90};
 //states
 /*
 struct automat_bereit{};
@@ -31,55 +46,178 @@ struct kontostand{};
 
 //events
 struct karte_eingef{
-    string karte = "";
+    //std::string karte = "";
 };
 struct pin{
-    int pin = 0;
+    //int pin = 0;
 };
 struct abbruch{
-    bool value = true;
+    //bool value = true;
 };
-struct falscher_pin{
-    int wrong_pin = 0;
-};
+
 struct geld_abheben_e{
-    bool geld = true;
+    //bool geld = true;
 };
 struct kontostand_e{
-    bool kontostand = true;
+   //bool kontostand = true;
 };
 struct x_euro{
-    int euro = 0;
+    //int euro = 0;
 };
 struct weitere_aktion{
-    bool action = true;
+    //bool action = true;
 };
+
+//guards
+
+struct Karte{
+    int pin;
+};
+
+struct PIN {
+    int value;
+};
+
+const auto right_PIN = [](PIN& pin, Karte& karte){
+    std::cout << "PIN VALUE: " << pin.value << std::endl;
+    return pin.value == karte.pin;
+};
+
+PIN pin_inp{};
+Karte karte_pin{};
 
 struct Automaton {
 
 public:
-  auto operator()() const {
+  auto operator()() {
     using namespace sml;
     return make_transition_table(
-     *"automat_bereit"_s + event<karte_eingef> = "erwarte_pin"_s,
-      "erwarte_pin"_s + event<pin> = "aktion_auswahlen"_s,
-      "erwarte_pin"_s + event<abbruch> = "automat_bereit"_s,
-      "erwarte_pin"_s + event<falscher_pin> = "falscher_pin1"_s,
-      "falscher_pin1"_s + event<falscher_pin> = "falscher_pin2"_s,
-      "falscher_pin1"_s + event<pin> = "aktion_auswahlen"_s,
-      "falscher_pin2"_s + event<falscher_pin>  = "automat_bereit"_s,
-      "falscher_pin2"_s + event<pin> = "falscher_pin2"_s,
-      "aktion_auswahlen"_s + event<geld_abheben_e> = "geld_abheben"_s,
-      "aktion_auswahlen"_s + event<kontostand_e> = "kontostand"_s,
-      "aktion_auswahlen"_s + event<abbruch> = "automat_bereit"_s,
-      "geld_abheben"_s + event<x_euro> = "karte_ausgegeben"_s,
-      "geld_abheben"_s + event<abbruch> = "automat_bereit"_s,
-      "karte_ausgegeben"_s + event<x_euro> = "automat_bereit"_s,
-      "kontostand"_s + event<weitere_aktion> = "aktion_auswahlen"_s,
-      "kontostand"_s + event<abbruch> = "automat_bereit"_s
+     *"automat_bereit"_s + event<karte_eingef> / [] {std::cout << "Neuer State: erwarte PIN" << std::endl << std::endl;} = "erwarte_pin"_s,
+      "automat_bereit"_s + on_exit<_> / [] {
+          std::cout << "Karte bitte!" << std::endl;
+          std::cin >> karte;
+
+          bool check_card{true};
+
+          while(check_card){
+        
+            if(cards.find(karte) == cards.end()){
+                std::cout << "Ungülitge Karte. Karte bitte!" << std::endl;
+                std::cin >> karte;
+            }
+            else{
+                karte_pin.pin = cards.at(karte);
+                std::cout << karte_pin.pin;
+                check_card=false;
+            }
+          }
+        
+      },
+      "erwarte_pin"_s + event<pin> [right_PIN] / [] {std::cout << "Neuer State: Aktion auswählen" << std::endl << std::endl;} = "aktion_auswahlen"_s,
+      "erwarte_pin"_s + event<abbruch> / [] {std::cout << "Neuer State: Automat bereit" << std::endl << std::endl;} = "automat_bereit"_s,
+      "erwarte_pin"_s + event<pin> [!right_PIN] / [] {std::cout << "Neuer State: Falscher PIN 1" << std::endl << std::endl;} = "falscher_pin1"_s,
+      "erwarte_pin"_s + on_entry<_> / [] {
+          std::cout << "PIN eingeben!" << std::endl;
+          std::cin >> pin_;
+          std::cout << pin_ << std::endl;
+          pin_inp.value = pin_;
+          std::cout << pin_inp.value << std::endl;
+        
+      },
+      "falscher_pin1"_s + event<pin> [!right_PIN] / [] {std::cout << "Neuer State: Falscher PIN 2" << std::endl << std::endl;} = "falscher_pin2"_s,
+      "falscher_pin1"_s + event<pin> [right_PIN] / [] {std::cout << "Neuer State: Aktion auswählen" << std::endl << std::endl;} = "aktion_auswahlen"_s,
+      "falscher_pin1"_s + on_entry<_> / [] {
+          std::cout << "Fehler 1! PIN erneut eingeben!" << std::endl;
+          std::cin >> pin_;
+          pin_inp.value = pin_;
+      },
+      "falscher_pin2"_s + event<pin> [!right_PIN] / [] {std::cout << "Neuer State: Automat bereit" << std::endl << std::endl;} = "automat_bereit"_s,
+      "falscher_pin2"_s + event<pin> [right_PIN] / [] {std::cout << "Neuer State: Aktion auswählen" << std::endl << std::endl;} = "aktion_auswahlen"_s,
+      "falscher_pin2"_s + on_entry<_> / [] {
+          std::cout << "Fehler 2! PIN erneut eingeben!" << std::endl;
+          std::cin >> pin_;
+          pin_inp.value = pin_;
+      },
+      "aktion_auswahlen"_s + event<geld_abheben_e> / [] {std::cout << "Neuer State: Geld abheben" << std::endl << std::endl;} = "geld_abheben"_s,
+      "aktion_auswahlen"_s + event<kontostand_e> / [] {std::cout << "Neuer State: Kontostand" << std::endl << std::endl;} = "kontostand"_s,
+      "aktion_auswahlen"_s + event<abbruch> / [] {std::cout << "Neuer State: Automat bereit" << std::endl << std::endl;} = "automat_bereit"_s,
+      "aktion_auswahlen"_s + on_entry<_> / [] {
+          std::cout << "Aktion auswählen! 1. Geld abheben oder 2. Kontostand anzeigen" << std::endl;
+          std::cin >> aktion;
+      },
+      "geld_abheben"_s + event<x_euro> / [] {std::cout << "Neuer State: Karte ausgegeben" << std::endl << std::endl;} = "karte_ausgegeben"_s,
+      "geld_abheben"_s + event<abbruch> / [] {std::cout << "Neuer State: Automat bereit" << std::endl << std::endl;} = "automat_bereit"_s,
+      "geld_abheben"_s + on_entry<_> / [] {
+
+          std::cout << "Wie viel wollen Sie abheben?" << std::endl;
+          std::cin >> geldmenge;
+
+          if(karte == "karte1"){
+                if(kontostand1 - geldmenge < 0){
+                    std::cout << "Nicht genügend Geld am Konto!" << std::endl;
+                    
+                }
+                else{
+
+                    kontostand1 -= geldmenge;
+
+                    std::cout << "Karte wird ausgegeben!" << std::endl;
+
+                    std::cout << "Ausgabe: " << geldmenge << std::endl;
+
+                }
+            }
+            else if(karte == "karte2"){
+                if(kontostand2 - geldmenge < 0){
+                    std::cout << "Nicht genügend Geld am Konto!" << std::endl;
+                    
+                }
+                else{
+
+                    kontostand2 -= geldmenge;
+
+                    std::cout << "Karte wird ausgegeben!" << std::endl;
+
+                    std::cout << "Ausgabe: " << geldmenge << std::endl;
+
+
+                }
+            }
+            else if(karte == "karte3"){
+                if(kontostand3 - geldmenge < 0){
+
+                    std::cout << "Nicht genügend Geld am Konto!" << std::endl;
+                        
+                }
+                else{
+
+                    kontostand3 -= geldmenge;
+
+                    std::cout << "Karte wird ausgegeben!" << std::endl;
+
+                    std::cout << "Ausgabe: " << geldmenge << std::endl;
+
+                }
+            }
+        },
+      "karte_ausgegeben"_s + event<x_euro> / [] {std::cout << "Neuer State: Automat bereit" << std::endl << std::endl;} = "automat_bereit"_s,
+      "kontostand"_s + event<weitere_aktion> / [] {std::cout << "Neuer State: Aktion auswählen" << std::endl << std::endl;} = "aktion_auswahlen"_s,
+      "kontostand"_s + event<abbruch> / [] {std::cout << "Neuer State: Automat bereit" << std::endl << std::endl;} = "automat_bereit"_s,
+      "kontostand"_s + on_entry<_> / [] {
+        if(karte == "karte1"){
+            std::cout << kontostand1 << "€" << std::endl;
+        }
+        else if(karte == "karte2"){
+            std::cout << kontostand2 << "€" << std::endl;
+        }
+        else if(karte == "karte3"){
+            std::cout << kontostand3 << "€" << std::endl;
+        }
+      }
     );
   }
 
 
 };
 }
+
--- code/src/main.cpp
+++ code/src/main.cpp
@@ -7,6 +7,7 @@
 #include "automaton.h"
 #include <map>
 
+using namespace std;
 
 namespace sml = boost::sml;
 
@@ -14,146 +15,206 @@ int main(){
 
   using namespace sml;
 
-  std::map<string, int> cards = {
-    { "karte1", 1234 },
-    { "karte2", 6969 },
-    { "karte3", 0420 }
-};
+  sm<Automaton> sm{pin_inp, karte_pin};
 
-  string karte{};
-  int pin_{};
-  int aktion{};
-  int geldmenge{};
+  while(true){
 
-  float kontostand1{69420};
-  float kontostand2{187};
-  float kontostand3{99.90};
+    sm.process_event(karte_eingef{});
 
-  bool run{true};
+    sm.process_event(pin{});
 
-  sm<Automaton> sm;
-
-  while(run){
-    cout << "Karte bitte!" << endl;
-    cin >> karte;
-
-    if(cards.find(karte) == cards.end()){
-      cout << "Falsche Karte!" << endl;
-
-      sm.process_event(abbruch{true});
-      continue;
+    if(aktion == 1){
+      sm.process_event(geld_abheben_e{});
+      sm.process_event(x_euro{});
+      sm.process_event(x_euro{});
     }
     else{
-
-      sm.process_event(karte_eingef{karte});
-
-
-      cout << "PIN eingeben!" << endl;
-      cin >> pin_;
-
-      try{
-        if(cards.at(karte) == pin_){
-
-          sm.process_event(pin{pin_});
-
-          cout << "Aktion auswählen! 1. Geld abheben oder 2. Kontostand anzeigen" << endl;
-          cin >> aktion;
-
-          if (aktion == 1){
-            sm.process_event(geld_abheben_e{true});
-
-            cout << "Wie viel wollen Sie abheben?" << endl;
-            cin >> geldmenge;
-
-            if(karte == "karte1"){
-              if(kontostand1 - geldmenge < 0){
-                cout << "Nicht genügend Geld am Konto!" << endl;
-                continue;
-              }
-              else{
-
-                kontostand1 -= geldmenge;
-
-                sm.process_event(x_euro{geldmenge});
-                cout << "Karte wird ausgegeben!" << endl;
-
-                sm.process_event(x_euro{geldmenge});
-                cout << "Ausgabe: " << geldmenge << endl;
-
-                sm.process_event(abbruch{true});
-                continue;
-              }
-            }
-            else if(karte == "karte2"){
-              if(kontostand2 - geldmenge < 0){
-                cout << "Nicht genügend Geld am Konto!" << endl;
-                continue;
-              }
-              else{
-
-                kontostand2 -= geldmenge;
-
-                sm.process_event(x_euro{geldmenge});
-                cout << "Karte wird ausgegeben!" << endl;
-
-                sm.process_event(x_euro{geldmenge});
-                cout << "Ausgabe: " << geldmenge << endl;
-
-                sm.process_event(abbruch{true});
-                continue;
-              }
-            }
-            else if(karte == "karte3"){
-              if(kontostand3 - geldmenge < 0){
-                cout << "Nicht genügend Geld am Konto!" << endl;
-                continue;
-              }
-              else{
-
-                kontostand3 -= geldmenge;
-
-                sm.process_event(x_euro{geldmenge});
-                cout << "Karte wird ausgegeben!" << endl;
-
-                sm.process_event(x_euro{geldmenge});
-                cout << "Ausgabe: " << geldmenge << endl;
-
-                sm.process_event(abbruch{true});
-                continue;
-              }
-            }
-
-          }
-          else if(aktion == 2){
-
-            sm.process_event(kontostand_e{true});
-
-            if(karte == "karte1"){
-              cout << kontostand1 << "€" << endl;
-            }
-            else if(karte == "karte2"){
-              cout << kontostand2 << "€" << endl;
-            }
-            else if(karte == "karte3"){
-              cout << kontostand3 << "€" << endl;
-            }
-
-            sm.process_event(abbruch{true});
-            continue;
-          }
-
-        }
-        else{
-          sm.process_event(falscher_pin{});
-          run = false;
-        }
-
-      }
-      catch(const std::exception& e){
-        cerr << "Unerwarteter Fehler aufgetreten!" << endl;
-      }
-      
+      sm.process_event(kontostand_e{});
     }
   }
+
 }
 
+
+
+
+
+
+
+
+
+
+
+
+/*int main(){
+
+    using namespace sml;
+
+    std::map<string, int> cards = {
+        { "karte1", 1234 },
+        { "karte2", 6969 },
+        { "karte3", 0420 }
+    };
+
+    string karte{};
+    int pin_{};
+    int aktion{};
+    int geldmenge{};
+
+    float kontostand1{69420};
+    float kontostand2{187};
+    float kontostand3{99.90};
+
+    bool run{true};
+
+    
+
+    sm<Automaton> sm{karte_inp, pin_inp};
+
+    while(run){
+
+        cout << "Karte bitte!" << endl;
+        cin >> karte;
+        cout << endl << endl;
+
+        if(cards.find(karte) == cards.end()){
+          cout << "Falsche Karte!" << endl;
+
+          sm.process_event(abbruch{true});
+          continue;
+        }
+        else{
+
+          sm.process_event(karte_eingef{karte});
+
+          karte_inp.pin = cards.at(karte);
+
+          cout << "PIN eingeben!" << endl;
+          cin >> pin_;
+          cout << endl << endl;
+
+          try{
+
+            //if(cards.at(karte) == pin_){
+
+            pin_inp.value = pin_;
+
+            sm.process_event(pin{pin_});
+
+            cout << "Aktion auswählen! 1. Geld abheben oder 2. Kontostand anzeigen" << endl;
+            cin >> aktion;
+
+            if (aktion == 1){
+                sm.process_event(geld_abheben_e{true});
+
+                cout << "Wie viel wollen Sie abheben?" << endl;
+                cin >> geldmenge;
+
+                if(karte == "karte1"){
+                if(kontostand1 - geldmenge < 0){
+                    cout << "Nicht genügend Geld am Konto!" << endl;
+                    continue;
+                }
+                else{
+
+                    kontostand1 -= geldmenge;
+
+                    sm.process_event(x_euro{geldmenge});
+                    cout << "Karte wird ausgegeben!" << endl;
+
+                    sm.process_event(x_euro{geldmenge});
+                    cout << "Ausgabe: " << geldmenge << endl;
+
+                    sm.process_event(abbruch{true});
+                    continue;
+                }
+                }
+                else if(karte == "karte2"){
+                if(kontostand2 - geldmenge < 0){
+                    cout << "Nicht genügend Geld am Konto!" << endl;
+                    continue;
+                }
+                else{
+
+                    kontostand2 -= geldmenge;
+
+                    sm.process_event(x_euro{geldmenge});
+                    cout << "Karte wird ausgegeben!" << endl;
+
+                    sm.process_event(x_euro{geldmenge});
+                    cout << "Ausgabe: " << geldmenge << endl;
+
+                    sm.process_event(abbruch{true});
+                    continue;
+                }
+                }
+                else if(karte == "karte3"){
+                if(kontostand3 - geldmenge < 0){
+                    cout << "Nicht genügend Geld am Konto!" << endl;
+                    continue;
+                }
+                else{
+
+                    kontostand3 -= geldmenge;
+
+                    sm.process_event(x_euro{geldmenge});
+                    cout << "Karte wird ausgegeben!" << endl;
+
+                    sm.process_event(x_euro{geldmenge});
+                    cout << "Ausgabe: " << geldmenge << endl;
+
+                    sm.process_event(abbruch{true});
+                    continue;
+                }
+                }
+
+            }
+            else if(aktion == 2){
+
+                sm.process_event(kontostand_e{true});
+
+                if(karte == "karte1"){
+                cout << kontostand1 << "€" << endl;
+                }
+                else if(karte == "karte2"){
+                cout << kontostand2 << "€" << endl;
+                }
+                else if(karte == "karte3"){
+                cout << kontostand3 << "€" << endl;
+                }
+
+                sm.process_event(abbruch{true});
+                continue;
+            }
+
+            }
+            else{
+              sm.process_event(falscher_pin{});
+              
+            }
+
+        }
+        catch(const std::exception& e){
+            cerr << "Unerwarteter Fehler aufgetreten!" << endl;
+        }
+        
+        }
+    }
+}
+
+void check_card(string karte){
+
+    if(karte != "karte1" && karte != "karte2" && karte != "karte3"){
+
+        cout << "Karte bitte!" << endl;
+        cin >> karte;
+
+        check_card(karte);
+    }
+    else{
+        return;
+    }
+
+
+
+}*/
